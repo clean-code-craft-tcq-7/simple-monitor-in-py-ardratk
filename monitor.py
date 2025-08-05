cb6edfe4023a051ce358924919364929f8159e5a
Thaivalappil Kunjumon Ardra (MS/EHM-BST-TS) <xhi1cob@bosch.com>
2025-08-05 13:55:03 +0530

    Reduced complexity

--- monitor.py
+++ monitor.py
@@ -1,37 +1,39 @@
-
 from time import sleep
 import sys
 
+def check_temperature(temperature):
+    if temperature > 102 or temperature < 95:
+        return False, 'Temperature critical!'
+    return True, ''
+
+def check_pulse(pulseRate):
+    if pulseRate < 60 or pulseRate > 100:
+        return False, 'Pulse Rate is out of range!'
+    return True, ''
+
+def check_spo2(spo2):
+    if spo2 < 90:
+        return False, 'Oxygen Saturation out of range!'
+    return True, ''
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    checks = [
+        check_temperature(temperature),
+        check_pulse(pulseRate),
+        check_spo2(spo2)
+    ]
+    for ok, msg in checks:
+        if not ok:
+            alert(msg)
+            return False
+    return True
+
+def alert(message):
+    print(message)
+    for _ in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
